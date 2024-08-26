8e3c1d25c0e9c40748187c1f9f14a7100393e57a
Aksha malik <aksha5u786@gmail.com>
2024-08-26 23:43:16 +0530

    first commit done till popular products

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+export {
+    Button,
+    Nav,
+    ShoeCard,
+    PopularProductCard,
+}
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+
+export {
+    Hero,
+    Subscribe,
+    SuperQuality,
+    Services,
+    PopularProducts,
+    Footer,
+    CustomerReviews,
+    SpecialOffer
+}
