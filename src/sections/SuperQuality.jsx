bbaca38f6089510cf66c497504b736bb1c0789fc
Aksha malik <aksha5u786@gmail.com>
2024-08-27 16:32:20 +0530

    included product and specialoffer

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 import { Button } from '../components'
+import { shoe8 } from '../assets/images'
 const SuperQuality = () => {
   return (
     <section id="about-us" className='flex justify-between items-center max-lg:flex-col gap-10 w-full max-container'>
      <div className='flex flex-1 flex-col'>
-     <p className='text-xl font-montserrat text-coral-red'>
-          Our Summer collections
+        <h2 className='font-palanquin text-4xl captalize font-bold lg:max-w-lg'>
+          We Provide You
+          <span className='text-coral-red'>Super</span><span className='text-coral-red'>Quality</span>Shoes
+        </h2>
+        <p className='mt-4 lg:max-w-lg info-text'>
+          Ensuring premium comfort and style, our meticulously crafted footwear is designed to elevate your experience,providing you with unmatched quality ,innovations, and a touch of elegance.
         </p>
-
-        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
-          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
-            The New Arrival
-          </span>
-          <br />
-          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
-        </h1>
-        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
-          Discover stylish Nike arrivals, quality comfort, and innovation for
-          your active life.
+        <p className='mt-6 lg:max-w-lg info-text'>
+          Our dedication to detail and excellence ensures your satisfaction
         </p>
-
-        <Button label='Shop now' iconURL={arrowRight} />
+        <div className='mt-11'><Button label='View Details'/></div>
+     </div>
+     <div className='flex-1 flex justify-center items-center'>
+       <img src={shoe8} alt="shoe8" width={578} height={522} className='object-contain' />
      </div>
     </section>
   )
